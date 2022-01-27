aac5983352226ab43f53cf90420899dde985b043
xyzuka <94155478+xyzuka@users.noreply.github.com>
2022-01-27 12:06:28 +1100

    Eraser Button Bug Fixed

--- app.js
+++ app.js
@@ -15,15 +15,38 @@ let defaultColor = "black";
 // Function that takes size (rows * columns) and makes a grid
 function createGrid(size) {
     for (let i = 0; i < (size * size); i++) {
+        //Creates new div elements
         const div = document.createElement('div');
         sketchbox.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
         sketchbox.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+        //Adding the new divs to the sketchbox container
         sketchbox.appendChild(div).classList.add('grid');
     }
 }
 
 // Default size
 createGrid(size); 
+
+/* ------- NEED HELP HERE --------- */
+// Resize Sketchbox button
+resizeBtn.addEventListener("click", function() {
+    let input = prompt("Please enter new dimensions of sketchbox");
+    if (input <= 100) {
+      // Function to resize the sketch box
+      createGrid(input);
+      clearGrid();
+    } else {
+      prompt("Please enter a number under 100");
+    }
+  })
+  /* ------- NEED HELP HERE --------- */
+
+
+
+
+
+
+
 
 // Hovering mouse will change color of div to BLACK
 const blackMode = function() {
@@ -35,16 +58,17 @@ const blackMode = function() {
         })
     })
 }
-
 blackMode();
 
 // Rainbow button
 rainBowBtn.addEventListener("click", (e) => {
     if (rainBowBtn.textContent === "Rainbow Mode Off") {
         rainBowBtn.textContent = "Rainbow Mode On";
+        eraserBtn.textContent = "Eraser Off";
         rainBowMode();
     } else {
         rainBowBtn.textContent = "Rainbow Mode Off";
+        eraserBtn.textContent = "Eraser Off";
         blackMode();
     }
 })
@@ -77,12 +101,17 @@ eraserBtn.addEventListener("click", (e) => {
     // Enable Eraser
     if (eraserBtn.textContent === "Eraser Off") {
         eraserBtn.textContent = "Eraser On";
-    // Disable Eraser
+        eraseGrid();
+    // Disable Eraser and enable color button mode
     } else {
-        eraserBtn.textContent = "Eraser Off";
-    }
-    eraseGrid();
-})
+        if (rainBowBtn.textContent === "Rainbow Mode Off") {
+            blackMode();
+            eraserBtn.textContent = "Eraser Off";
+        } else {
+            rainBowMode();
+            eraserBtn.textContent = "Eraser Off";
+        }
+    }})
 
 // Clear Function
 const clearGrid = function() {
@@ -101,7 +130,10 @@ clearBtn.addEventListener("click", clearGrid);
 
 
 
-// PROJECT NOTES/ LESSONS LEARNT 
+
+/* ------  PROJECT NOTES / LESSONS LEARNT ------ */
 
 // 1. Was stuck on the Eraser button functionality/toggle feature because I initially used = first in my if statement and = is only used for assigning values eg. let zero = 0; whereas I was meant to use === for comparisons
+
+// 2. Manually adding inline styles for DOM Manipulation rather than toggling classes seems to make if else functions less confusing
 
